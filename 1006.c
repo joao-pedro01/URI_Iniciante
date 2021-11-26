56c31d94baebada7f4d65a52f5dcf0a9563c43f7
João Pedro <80638211+joao-pedro01@users.noreply.github.com>
2021-11-26 09:25:42 -0300

    Add files via upload

--- 1006.c
+++ 1006.c
@@ -1,6 +1,5 @@
 /*
 	Leia 3 valores, no caso, variáveis A, B e C, que são as três notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 2, a nota B tem peso 3 e a nota C tem peso 5. Considere que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
-
 	Entrada
 	O arquivo de entrada contém 3 valores com uma casa decimal, de dupla precisão (double).
 	
@@ -19,7 +18,7 @@ int main() {
 	
 	Media = ((A * 2) + (B * 3) + (C * 5)) / 10;
 	
-	printf ("DIFERENCA = %lf\n", Media);
+	printf ("MEDIA = %.1lf\n", Media);
 	
 	return 0;
 }
--- 1015.c
+++ 1015.c
@@ -14,12 +14,14 @@
 
 int main() {
  
-    float X1, Y1, X2, Y2;
+    float X1, Y1, X2, Y2, Dist;
 
-    scanf ("%f %f", X1, Y1);
-    scanf ("%f %f", X2, Y2);
+    scanf ("%f %f", &X1, &Y1);
+    scanf ("%f %f", &X2, &Y2);
 
-    sqrt((X2-X1)pow() );
- 
-    return 0;
+    Dist = sqrt(pow((X2-X1),2) + pow((Y2-Y1),2));
+
+    printf ("%.4f\n", Dist);
+
+   return 0;
 }
